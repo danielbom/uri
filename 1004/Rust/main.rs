c689eeb401f8bb5634e771eaa4bae4fa3562d58e
danielbom <daniel.rodrigues.45@hotmail.com>
2021-08-18 21:11:27 -0300

    Add 1004

---
+++ 1004/Rust/main.rs
@@ -0,0 +1,17 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1000
+use std::io;
+
+fn read_i32() -> io::Result<i32> {
+  let mut line = String::new();
+  io::stdin().read_line(&mut line)?;
+  let x = line.trim_end().parse::<i32>().unwrap();
+  Ok(x)
+}
+
+fn main() -> io::Result<()> {
+  let x = read_i32()?;
+  let y = read_i32()?;
+  let product = x * y;
+  println!("PROD = {}", product);
+  Ok(())
+}
