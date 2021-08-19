215e90ee7124c1e8bbfcca3d3277c6eb17da4c90
danielbom <daniel.rodrigues.45@hotmail.com>
2021-08-18 21:21:07 -0300

    Add 1005

---
+++ 1005/Rust/main.rs
@@ -0,0 +1,17 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1000
+use std::io;
+
+fn read_f64() -> io::Result<f64> {
+  let mut line = String::new();
+  io::stdin().read_line(&mut line)?;
+  let x = line.trim_end().parse::<f64>().unwrap();
+  Ok(x)
+}
+
+fn main() -> io::Result<()> {
+  let grade_a = read_f64()?;
+  let grade_b = read_f64()?;
+  let average = (grade_a * 3.5 + grade_b * 7.5) / 11.0;
+  println!("MEDIA = {:.5}", average);
+  Ok(())
+}
