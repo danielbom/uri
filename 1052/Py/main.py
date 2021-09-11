61e251c27bd8276de111fff2bc3ca3e04cc519d5
danielbom <daniel.rodrigues.45@hotmail.com>
2021-09-10 21:24:31 -0300

    Add 1052

---
+++ 1052/Py/main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+]
+
+x = int(input())
+
+print(months[x - 1])
