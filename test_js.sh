9769c1d0eaecd692f7cda54f33278e0ef87202a7
danielbom <daniel.rodrigues.45@hotmail.com>
2021-09-12 20:05:48 -0300

    Add javascript version on 1000

---
+++ test_js.sh
@@ -0,0 +1,26 @@
+function dotest() {
+  echo "🚩 Test $1"
+  echo "Input:"
+  cat ../inputs/$1
+  node main.js > .output < "../inputs/$1"
+  echo "Result:"
+  cat .output
+  echo "" 
+
+  ok=`diff .output ../outputs/$1`
+  if [ ! -z "$ok" ]
+  then
+    echo "$ok"
+    exit 1
+  fi
+  rm .output
+}
+
+echo "🏁 Start tests"
+
+for filename in ../inputs/*.txt
+do
+  dotest `basename $filename`
+done
+
+echo "🥳 Complete!"
