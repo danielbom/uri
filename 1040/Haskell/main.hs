d97acef712c2708f214cb342071f34026018729c
danielbom <daniel.rodrigues.45@hotmail.com>
2021-09-25 11:40:57 -0300

    Update 1040

--- 1040/Haskell/main.hs
+++ 1040/Haskell/main.hs
@@ -1,8 +1,9 @@
 import Text.Printf
 
 data Score = Approved | Reproved | InExame
+    deriving (Eq)
 
-scoreType1 avg  
+scoreType1 avg 
     | avg >= 7.0 = Approved
     | avg < 5.0  = Reproved
     | otherwise  = InExame
@@ -11,29 +12,28 @@ scoreType2 avg
     | avg < 5.0 = Reproved
     | otherwise = Approved
 
+scoreToStr score = case score of
+    Approved -> "Aluno aprovado."
+    Reproved -> "Aluno reprovado."
+    InExame  -> "Aluno em exame."
+
 showResult (firstLine:xs) =
-    case scores of
-        (s1:s2:s3:s4) -> unlines $
-            [printf "Media: %.1f" average1] ++
-            case scoreType1 average1 of
-                Approved -> ["Aluno aprovado."]
-                Reproved -> ["Aluno reprovado."]
-                InExame  ->
-                    case xs of
-                        [exameScore] -> 
-                            [ "Aluno em exame."
-                            , "Nota do exame: " ++ exameScore
-                            ] ++
-                            case scoreType2 average2 of
-                                Approved -> ["Aluno aprovado.", finalScoreText]
-                                Reproved -> ["Aluno reprovado.", finalScoreText]
-                            where average2 = (read exameScore + average1) / 2.0 
-                                  finalScoreText = printf "Media final: %.1f" average2
-                        _ -> ["Aluno reprovado."]
-        _ -> []
-    where scores = (map read . words $ firstLine) :: [Double]
-          weights = [2.0, 3.0, 4.0, 1.0]
-          average1 = (sum $ zipWith (*) weights scores) / (sum weights)
+    let scores = (map read . words $ firstLine) :: [Double]
+        weights = [2.0, 3.0, 4.0, 1.0]
+        average1 = (sum $ zipWith (*) weights scores) / (sum weights)
+        st1 = scoreType1 average1
+        exameScore = head $ xs
+        average2 = (read exameScore + average1) / 2.0 
+        exameMsg = 
+            if st1 == InExame && not (null xs)
+                then [ "Nota do exame: " ++ exameScore
+                     , scoreToStr (scoreType2 average2)
+                     , printf "Media final: %.1f" average2
+                     ]
+                else []
+    in  [ printf "Media: %.1f" average1
+        , scoreToStr st1
+        ] ++ exameMsg
 showResult _ = []
 
-main = interact (showResult . lines)
+main = interact (unlines . showResult . lines)
