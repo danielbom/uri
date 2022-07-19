657cc208b553870faf09d4f25c86a0d869cf9b63
danielbom <daniel.rodrigues.45@hotmail.com>
2022-07-18 21:42:35 -0300

    Add 1187

---
+++ 1187/Haskell/main.hs
@@ -0,0 +1,23 @@
+-- https://www.beecrowd.com.br/judge/en/problems/view/1187
+import Control.Monad
+import Text.Printf
+
+average xs = (sum xs) / (fromIntegral $ length xs)
+
+topArea :: [Double] -> [Double]
+topArea xs = concat $ go xs (Left 1)
+    where
+        go [] _ = []
+        go ys (Left i) = go (drop i ys) (Right (i + 1))
+        go ys (Right i) = start' : go end' (Left (i + 1))
+            where (start', end') = splitAt (12 - i) ys
+
+main = do
+    op <- getLine
+    xs <- replicateM 144 readLn 
+    let ys = topArea xs
+
+    if op == "M" then
+        putStr $ printf "%.1f\n" (average ys :: Double)
+    else
+        putStr $ printf "%.1f\n" (sum ys :: Double)
