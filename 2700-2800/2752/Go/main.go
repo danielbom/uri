196ccade6e4f8622ccbced88308634a814b85997
danielbom <daniel.rodrigues.45@hotmail.com>
2024-09-27 00:52:36 -0300

    Add 2752

---
+++ 2700-2800/2752/Go/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	message := "AMO FAZER EXERCICIO NO URI"
+	fmt.Printf("<% s>\n", message)
+	fmt.Printf("<% 30s>\n", message)
+	fmt.Printf("<% .20s>\n", message)
+	fmt.Printf("<% -20s>\n", message)
+	fmt.Printf("<% -30s>\n", message)
+	fmt.Printf("<% .30s>\n", message)
+	fmt.Printf("<% 30.20s>\n", message)
+	fmt.Printf("<% -30.20s>\n", message)
+}
